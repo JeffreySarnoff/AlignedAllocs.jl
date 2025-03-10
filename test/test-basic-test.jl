c918a51751624773a840e1569e29003bd55de38a
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-03-09 22:19:23 -0400

    update test

--- test/test-basic-test.jl
+++ test/test-basic-test.jl
@@ -1,9 +1,10 @@
 @testset "AlignedAllocs.jl" begin
+    @test AlignedAllocs.CACHE_LINE_SIZE >= 32
     vec = memalign(Float32, 128, 64)
     zvec = memalign_clear(UInt16, 256, 64)
     @test length(vec) == 128
     @test length(zvec) == 256
     @test zvec[1] == 0
-    @test UInt64(pointer(vec)) % 64 == 0
-    @test UInt64(pointer(zvec)) % 64 == 0
+    @test UInt64(pointer(vec)) % AlignedAllocs.CACHE_LINE_SIZE == 0
+    @test UInt64(pointer(zvec)) % AlignedAllocs.CACHE_LINE_SIZE == 0
 end
