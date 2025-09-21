5b12c5706c617a4e01078f1ab6d535b736ae0f5a
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-09-21 01:32:24 -0400

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -45,3 +45,52 @@ end
     @test Base.ispow2(value)
     @test UInt(pointer(base)) % value == 0
 end
+
+
+@testset "Helper predicates" begin
+    @test AlignedAllocs._valid_alignment(16)
+    @test AlignedAllocs._valid_alignment(256)
+    @test !AlignedAllocs._valid_alignment(0)
+    @test !AlignedAllocs._valid_alignment(18)
+    @test !AlignedAllocs._valid_alignment(-32)
+end
+
+@testset "check_args validation" begin
+    @test isnothing(AlignedAllocs.check_args(UInt8, 1, 64))
+    @test_throws ArgumentError AlignedAllocs.check_args(UInt8, 0, 64)
+    @test_throws ArgumentError AlignedAllocs.check_args(UInt8, 4, 12)
+    @test_throws ArgumentError AlignedAllocs.check_args(String, 2, 64)
+end
+
+@testset "alloc_error translations" begin
+    @test isnothing(AlignedAllocs.alloc_error(Cint(0)))
+    @test_throws ArgumentError AlignedAllocs.alloc_error(AlignedAllocs.EINVAL)
+    @test_throws OutOfMemoryError AlignedAllocs.alloc_error(AlignedAllocs.ENOMEM)
+    err = @test_throws ErrorException AlignedAllocs.alloc_error(Cint(99))
+    @test occursin("99", err.value.msg)
+end
+
+@testset "_memzero! zeroes buffers" begin
+    buffer = fill(UInt8(0xff), 32)
+    GC.@preserve buffer begin
+        ptr = Base.unsafe_convert(Ptr{UInt8}, Base.pointer(buffer))
+        AlignedAllocs._memzero!(ptr, length(buffer))
+    end
+    @test all(iszero, buffer)
+end
+
+@testset "Large alignments" begin
+    align = 512
+    vect = memalign(UInt64, 4, align)
+    @test length(vect) == 4
+    addr = UInt(pointer(vect))
+    @test addr % align == 0
+    @test alignment(vect) >= align
+end
+
+@testset "Alignment on empty arrays" begin
+    empty_vec = Vector{Float64}(undef, 0)
+    value = alignment(empty_vec)
+    @test value >= 0
+    @test Base.ispow2(value) || value == 0
+end
