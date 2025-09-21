6867fad71024c01bf4f0cf5cb06672e443655762
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-09-21 04:08:18 -0400

    update

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,30 @@
-using Documenter
+using Documenter, AlignedAllocs
 
 makedocs(
     modules = [AlignedAllocs],
-    sitename = "AlignedAllocs.jl",
     authors = "Jeffrey Sarnoff",
+    repo = "https://github.com/JeffreySarnoff/AlignedAllocs.jl/blob/{commit}{path}#{line}",
+    sitename = "AlignedAllocs.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://jeffreysarnoff.github.io/AlignedAllocs.jl",
+        repolink = "https://github.com/JeffreySarnoff/AlignedAllocs.jl",
+        edit_link = "main",
+        assets = String[],
+        sidebar_sitename = false,
+    ),
     pages = [
         "Home" => "index.md",
         "User Guide" => "guide.md",
         "API Reference" => "reference.md",
-    ]
+    ],
+    checkdocs = :none,
+    linkcheck = true,
 )
 
-deploydocs(
+deploydocs(;
     repo = "github.com/JeffreySarnoff/AlignedAllocs.jl.git",
-    target = "build"
+    target = "build",
+    devbranch = "main",
+    push_preview = true,
 )
